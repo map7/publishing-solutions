3a55037c4f39498e65511ecae18b28169052fe95
Archimede Luca <archimede.luca@gmail.com>
2016-01-19 12:57:47 +1100

    added image on header

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( aaa.jpg )
 Rails.application.config.assets.precompile += %w( logo-color.png )
 Rails.application.config.assets.precompile += %w( logo-footer.png )
+Rails.application.config.assets.precompile += %w( weproduce.png )
