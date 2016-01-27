f95dea6a294194e825d21896856e0e8eaa669458
Michael Pope <michael@dtcorp.com.au>
2016-01-27 15:47:33 +1100

    Hacky fix for owl carousel
    The problem is the first carousel works for the first load, but then
    when we go back to the first genre it bumps the styling completely out
    just for the first one in the genres. I got around it by creating a
    blank for the first one.

--- vendor/extensions/backlists/app/controllers/refinery/backlists/backlists_controller.rb
+++ vendor/extensions/backlists/app/controllers/refinery/backlists/backlists_controller.rb
@@ -2,8 +2,7 @@ module Refinery
   module Backlists
     class BacklistsController < ::ApplicationController
 
-      before_action :find_all_backlists
-      before_action :find_all_genres
+      before_action :find_all_genres_including_all
       before_action :find_page
 
       def index
@@ -22,12 +21,32 @@ module Refinery
 
     protected
 
-    def find_all_genres
-      @genres = Genre.order('name ASC')
-    end
+    	def find_all_genres
+      	@genres = Genre.order('name ASC')
+    	end
 
       def find_all_backlists
         @backlists = Backlist.order('position ASC')
+      end
+
+      def find_all_genres_including_all
+        find_all_genres
+        find_all_backlists
+
+        @all_genres = [{name: "", backlists: []}]
+        @all_genres <<
+          {
+            name: "All",
+            backlists: @backlists
+          }
+        @genres.map{|g|
+          @all_genres << {
+            name: g.name,
+            backlists: g.backlists
+          }
+        }
+
+        @all_genres
       end
 
       def find_page
