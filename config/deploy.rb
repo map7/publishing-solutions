8779f174cd0a9ad905d8306cc49ae84f45bbfd9c
Michael Pope <michael@dtcorp.com.au>
2016-03-02 15:21:40 +1100

    change deployment to production VPS at ransomit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'publishing-solutions.com.au'
-set :repo_url, 'ssh://map7@192.168.200.4:22/~/code/publishing_solutions_website'
+set :repo_url, 'ssh://map7@mail.tramontana.com.au:2222/~/code/publishing_solutions_website'
 #set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '192.168.200.215', user: 'map7', roles: %w{app db web}
+#server '192.168.200.215', user: 'map7', roles: %w{app db web}
+server '163.47.16.141', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
