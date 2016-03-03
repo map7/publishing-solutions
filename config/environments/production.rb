fc45732f968fc3aba054ad91539d18f5d758e68d
Michael Pope <michael@dtcorp.com.au>
2016-03-03 11:35:11 +1100

    Remark out the gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,22 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@publishing-solutions.com.au'}
-  
-  if ENV['MAILER_USERNAME'].present? && ENV['MAILER_PASSWORD'].present?
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain:  "publishing-solutions.com.au",
-      user_name: "#{ENV['MAILER_USERNAME']}",
-      password: "#{ENV['MAILER_PASSWORD']}",
-      authentication: 'plain',
-      enable_starttls_auto: true
-    }
-  end
+
+  # --------------------------------------------------------------------------------
+  # Setup using gmail
+  # --------------------------------------------------------------------------------
+  # if ENV['MAILER_USERNAME'].present? && ENV['MAILER_PASSWORD'].present?
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address: "smtp.gmail.com",
+  #     port: 587,
+  #     domain:  "publishing-solutions.com.au",
+  #     user_name: "#{ENV['MAILER_USERNAME']}",
+  #     password: "#{ENV['MAILER_PASSWORD']}",
+  #     authentication: 'plain',
+  #     enable_starttls_auto: true
+  #   }
+  # end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
