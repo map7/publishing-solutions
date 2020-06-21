6d90c1019be90a43fb5d2edfdb96a17be529a1da
Michael Pope <map7777@gmail.com>
2020-06-21 22:46:24 +1000

    Cap-add rbenv & passenger to config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,22 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.14.1'
 
 set :application, 'publishing-solutions.com.au'
 set :repo_url, 'git@github.com:map7/publishing-solutions.git'
 #set :repo_url, 'git@example.com:me/my_repo.git'
+
+# Rbenv settings
+set :rbenv_type, :system #:user # or :system, depends on your rbenv setup
+
+# set ruby version from the file:
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+# in case you use fullstaq-ruby or have a different path for your ruby versions
+# set :rbenv_ruby_dir, '/usr/lib/fullstaq-ruby/versions' 
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -34,6 +47,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# Setup passenger
+set :passenger_restart_with_touch, true
 
 namespace :deploy do
   after :restart, :clear_cache do
